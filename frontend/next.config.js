93a0690fafddc1568a4d293d09bf9d48aea1e5b9
prxshant07 <prashantrajput7116@gmail.com>
2026-05-28 01:49:48 +0530

    next.js.config fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
+  },
 };
 
 module.exports = nextConfig;

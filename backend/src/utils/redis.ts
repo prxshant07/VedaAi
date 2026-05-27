86a0273127525161f90bbda0ba03f97b4884ba18
prxshant07 <prashantrajput7116@gmail.com>
2026-05-27 23:02:51 +0530

    redis deployment configuration

--- backend/src/utils/redis.ts
+++ backend/src/utils/redis.ts
@@ -13,7 +13,6 @@ export async function connectRedis(): Promise<void> {
     socket: {
       tls: true,
       reconnectStrategy: (retries) => Math.min(retries * 50, 2000),
-      keepAlive: 5000,
       connectTimeout: 10000,
     },
   }) as RedisClientType;

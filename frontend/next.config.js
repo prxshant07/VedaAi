208216a0c56ce9397777a4796d095ab32d3d75e7
prxshant07 <prashantrajput7116@gmail.com>
2026-05-28 12:45:19 +0530

    removed custom env logic from next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
-
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
-  },
 };
 
 module.exports = nextConfig;
